95fc4d9c0ad7eacc47d86deaee3fea6f9403cd7e
kuraga-2 <denismanarskij@tutamail.com>
2025-08-26 11:59:22 +0300

    polymorph_remaster

--- polymorf_lz/polymorph.py
+++ polymorf_lz/polymorph.py
@@ -4,10 +4,20 @@ class DF():
         self.name = pd.read_csv('polymorf_lz/var9.csv')
 
     def divide(self):
-        self.filtered_df_CB_true = self.name[self.name['Cash-back'] == "да" ]
-        self.filtered_df_CB_false = self.name[self.name['Cash-back'] == "нет"]
-        self.filtered_df_CB_true.to_csv('polymorf_lz/df_CB_true.csv', index=False)
-        self.filtered_df_CB_false.to_csv('polymorf_lz/df_CB_false.csv', index=False) 
+        self.filtered_df_1 = self.name[self.name['Место оплаты'] == "Брест" ]
+        self.filtered_df_2 = self.name[self.name['Место оплаты'] == "Витебск" ]
+        self.filtered_df_3 = self.name[self.name['Место оплаты'] == "Гомель" ]
+        self.filtered_df_4 = self.name[self.name['Место оплаты'] == "Гродно" ]
+        self.filtered_df_5 = self.name[self.name['Место оплаты'] == "Минск" ]
+        self.filtered_df_6 = self.name[self.name['Место оплаты'] == "Могилев" ]
+        
+
+        self.filtered_df_1.to_csv('polymorf_lz/df_1.csv', index=False)
+        self.filtered_df_2.to_csv('polymorf_lz/df_1.csv', index=False)
+        self.filtered_df_3.to_csv('polymorf_lz/df_1.csv', index=False)
+        self.filtered_df_4.to_csv('polymorf_lz/df_1.csv', index=False)
+        self.filtered_df_5.to_csv('polymorf_lz/df_1.csv', index=False)
+        self.filtered_df_6.to_csv('polymorf_lz/df_1.csv', index=False)
 
 
     def __invert__(self):
